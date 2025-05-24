040143233e9fdee3ff7c90a36502439e7889673f
Mayank-2705 <168637694+Retarded23@users.noreply.github.com>
2025-05-24 19:30:46 +0530

    Add files via upload
    data storage

--- backend/routes/crypto.routes.js
+++ backend/routes/crypto.routes.js
@@ -1,9 +1,27 @@
-import express from 'express';
-import { getCryptoData, getCryptoDataById } from '../controllers/crypto.controller.js';
+import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
-router.get('/get-all', getCryptoData);
-router.get('/:id', getCryptoDataById);
+router.get("/cryptos", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": "76a43e97-6e9d-4e78-a594-70cc94af5e85",
+        },
+        params: {
+          start: 1,
+          limit: 50,
+          convert: "USD",
+        },
+      }
+    );
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
