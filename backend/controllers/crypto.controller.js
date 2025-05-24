1a4068d39da8163984240a89654c5ea0d33aa03e
Mayank-2705 <168637694+Retarded23@users.noreply.github.com>
2025-05-24 19:32:04 +0530

    Add files via upload

--- backend/controllers/crypto.controller.js
+++ backend/controllers/crypto.controller.js
@@ -1,45 +1,32 @@
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const router = express.Router();
 const CMC_API_KEY = process.env.CMC_API_KEY
 const CMC_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency';
 
-export const getCryptoData = async (req, res) => {
-    try {
-        const response = await axios.get(`${CMC_API_URL}/listings/latest`, {
-            headers: {
-                'X-CMC_PRO_API_KEY': CMC_API_KEY,
-            },
-            params: {
-                start: '1',
-                limit: '100', // Adjust as needed
-                convert: 'USD', // Adjust as needed
-            },
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching crypto data:', error.response ? error.response.data : error.message);
-        res.status(500).json({ message: 'Failed to fetch cryptocurrency data', error: error.response ? error.response.data : error.message });
-    }
-};
+router.get("/cryptos", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${CMC_API_URL}/listings/latest`,
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": CMC_API_KEY,
+        },
+        params: {
+          start: 1,
+          limit: 10,
+          convert: "USD",
+        },
+      }
+    );
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-export const getCryptoDataById = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = await axios.get(`${CMC_API_URL}/quotes/latest`, {
-            headers: {
-                'X-CMC_PRO_API_KEY': CMC_API_KEY,
-            },
-            params: {
-                symbol: id.toUpperCase(),
-                convert: 'USD', // Adjust as needed
-            },
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error fetching crypto data by ID:', error.response ? error.response.data : error.message);
-        res.status(500).json({ message: 'Failed to fetch cryptocurrency data by ID', error: error.response ? error.response.data : error.message });
-    }
-}
+export default router;
